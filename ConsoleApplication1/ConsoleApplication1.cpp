c02a49225ead122f97c9249c913fc179e8c12afd
LawrenceBuckingham <lbuckingham@ozemail.com.au>
2022-08-04 11:56:05 +1000

    Example 1 done

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,43 +4,81 @@
 #include <cstdlib>
 #include <omp.h>
 
-#define N 1000000000
+#define N 2000
+
+double A[N][N], B[N][N], A_Orig[N][N], GroundTruth[N][N];
+
+void Populate(double x[N][N]) {
+    for (int i = 0; i < N; i++ ) {
+        for (int j = 0; j < N; j++) {
+            x[i][j] = 100.0 * rand() / RAND_MAX;
+        }
+    }
+}
+
+void Copy(double src[N][N], double dest[N][N]) {
+    for (int i = 0; i < N; i++ ) {
+        for (int j = 0; j < N; j++) {
+            dest[i][j] = src[i][j];
+        }
+    }
+}
+
+void Check(double src[N][N], double dest[N][N]) {
+    for (int i = 0; i < N; i++ ) {
+        for (int j = 0; j < N; j++) {
+            if (dest[i][j] != src[i][j]) {
+                printf("Error detected at (%d,%d)\n", i, j);
+                return;
+            }
+        }
+    }
+}
 
 int main()
 {
+    srand(12);
 
-    long long total = 0;
+    Populate(A);
+    Copy(A, A_Orig);
 
-    printf("sizeof(total) = %zu\n", sizeof(total));
-    printf("sizeof(long) = %zu\n", sizeof(long));
-    printf("sizeof(int) = %zu\n", sizeof(int));
-
-    // total = N * (N+1) / 2
+    Populate(B);
 
     auto start_time = omp_get_wtime();
-    for (int i = 0; i <= N; i++) {
-        total += i;
-    }
-    auto end_time = omp_get_wtime();
 
-    printf("total = %lld\n", total);
+    for(int i = 0;i< N;i++)
+        for(int j = 0;j < N;j++)
+            A[i][j] = A[i][j] + B[i][j];
+
+    auto end_time = omp_get_wtime();
+    Copy(A, GroundTruth);
+
     printf("Sequential time = %0.17f\n", (end_time - start_time));
 
-    long long par_total = 0;
-
-    auto par_start_time = omp_get_wtime();
+    Copy(A_Orig, A);
+    auto outer_start_time = omp_get_wtime();
 
 #pragma omp parallel for
-    for (int i = 0; i <= N; i++) {
-        auto reg = par_total;
-        reg = reg + i;
-        par_total = reg;
-        // par_total += i;
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            A[i][j] = A[i][j] + B[i][j];
+
+    auto outer_end_time = omp_get_wtime();
+
+    printf("Parallel outer time = %0.17f\n", (outer_end_time - outer_start_time));
+    Check(A, GroundTruth);
+
+    Copy(A_Orig, A);
+    auto inner_start_time = omp_get_wtime();
+
+    for (int i = 0; i < N; i++) {
+#pragma omp parallel for
+        for (int j = 0; j < N; j++) {
+            A[i][j] = A[i][j] + B[i][j];
+        }
     }
-    
-    auto par_end_time = omp_get_wtime();
 
-    printf("Parallel total = %lld\n", par_total);
-    printf("Parallel time = %0.17f\n", (par_end_time - par_start_time));
-
+    auto inner_end_time = omp_get_wtime();
+    printf("Parallel outer time = %0.17f\n", (outer_end_time - outer_start_time));
+    Check(A, GroundTruth);
 }
